2dc10fb31575b33d569e30017a968162da3e82e7
Bombastian1230 <92746366+Bombastian1230@users.noreply.github.com>
2026-03-19 12:55:19 +0100

    Added clear_up_to function

--- program/install/sFs/utils.lua
+++ program/install/sFs/utils.lua
@@ -137,4 +137,14 @@ function utils.yield(yeild_on, iteration)
     end
 end
 
+---Clears all the lines up to 'line'
+---@param line integer
+function utils.clear_up_to(line)
+    local _, start_y = term.getCursorPos()
+    for i = 1, line - start_y do
+        term.setCursorPos(1, start_y - i)
+        term.clearLine()
+    end
+end
+
 return utils
