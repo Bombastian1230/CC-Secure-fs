011136f0286fa43da189d5273cee0fa7ff97cb30
Bombastian1230 <92746366+Bombastian1230@users.noreply.github.com>
2026-03-23 09:20:55 +0100

    Added backup prng to crypto

--- program/install/sFs/crypto.lua
+++ program/install/sFs/crypto.lua
@@ -1,17 +1,14 @@
 local chacha20 = require("chacha20")
 local utils = require("utils")
+local sha256= require("sha256")
 
 local crypto = {}
 
-if not _G.__CRYPTO_STATE then
-    _G.__CRYPTO_STATE = {
-        pool_key = nil,
-        pool_key_gen_time = 0,
-        counter = 0
-    }
-end
-
-local state = _G.__CRYPTO_STATE
+local state = {
+    pool_key = nil,
+    pool_key_gen_time = 0,
+    counter = 0
+}
 
 ---Use random.org to generate a secure bytestring
 ---@param count any
@@ -34,24 +31,27 @@ end
 ---Generate a cryptograficly secure number
 ---@param length integer How many bytes (characters) long the string should be
 function crypto.random_bytes(length)
-    if settings.get("crypto.use_random_org", false) then
-        if state.pool_key == nil or os.epoch("utc") - state.pool_key_gen_time > 600000 then
-            local new_key = get_secure_bytes(32)
-            state.pool_key = new_key 
-            if state.pool_key == nil then goto failsafe end
-
+    print(state.counter)
+    if state.pool_key == nil or os.epoch("utc") - state.pool_key_gen_time > 600000 then
+        if settings.get("crypto.use_random_org", false) then
+            state.pool_key = get_secure_bytes(32)
             state.pool_key_gen_time = os.epoch("utc")
         end
-        
-        local random = chacha20.crypt(("\0"):rep(length), state.pool_key, "CSPRNG_NONCE", state.counter)
-        state.counter = state.counter + 1
 
-        return random
+        if state.pool_key == nil then
+            local entropy = tostring(os.epoch("utc")) ..
+                            tostring(math.random(1, 0xfffffff)) ..
+                            shell.getRunningProgram() ..
+                            tostring(os.clock()) ..
+                            tostring(os.computerID())
+            state.pool_key = sha256.hash(entropy)
+        end
     end
 
-    ::failsafe::
-    -- TODO: a backup prng
-    error("Couldn't generate number")
+    local random = chacha20.crypt(("\0"):rep(length), state.pool_key, "CSPRNG_NONCE", state.counter)
+    state.counter = state.counter + 1
+
+    return random
 end
 
 return crypto
